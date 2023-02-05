ef3b5054e0988a5ef7c4eeaaa364026f3188e880
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-02-05 20:25:00 +0200

    Added output of state with json

--- src/plugins/service-ats-controller/web.ts
+++ src/plugins/service-ats-controller/web.ts
@@ -84,162 +84,180 @@ export class Web {
   }
   private async getStates() {
     const self = this.uSelf;
-    await this._fastify.get("//", async (reply) => {
-      reply.header("content-type", "text/html");
-      let lines: Array<string> = ["<h1>ATS System</h1>", "<br />"];
+    await this._fastify.get(
+      "//",
+      async (reply, params, query): Promise<any | void> => {
+        if (!Tools.isNullOrUndefined(query) && query["json"] == "true") {
+          reply.header("content-type", "application/json");
+          return reply.send({
+            knownStates: self.knownStates,
+            inputs: self.inputs.getState(),
+            outputs: self.outputs.getState(),
+            loadShedding: self.loadSheddingState,
+          });
+        }
+        reply.header("content-type", "text/html");
+        let lines: Array<string> = ["<h1>ATS System</h1>", "<br />"];
 
-      const knownStates = self.knownStates as any;
-      lines.push(
-        '<div style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: flex-start; align-items: flex-start; align-content: flex-start; gap: 10px; ">'
-      );
-      lines.push(
-        "<style>.item-ball { width: 15px; height: 15px; border-radius: 50%; }</style>"
-      );
-      lines.push(
-        "<style>.item { background: white; border-radius: 10px; box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1); padding: 20px; }</style>"
-      );
-      lines.push(
-        "<style>.item h5 { display: block; margin: 0; margin-bottom: 10px; font-size: 20px; }</style>"
-      );
-      lines.push("<style>.item > br { display: none; }</style>");
-      lines.push(
-        "<style>.item > .litem { display: block; padding-bottom: 7px; }</style>"
-      );
-      lines.push(
-        "<style>.item > .litem > b, .item > .litem > span { display: inline-block; }</style>"
-      );
-      lines.push("<style>.item > .litem > span { float: right }</style>");
-      lines.push('<div class="item">');
-      lines.push("<h5>KNOWN STATES</h5>");
-      for (let key of Object.keys(knownStates)) {
-        let state: any = undefined;
-        if (Tools.isBoolean(knownStates[key])) {
-          state = `<div class="item-ball" style="background: ${
-            knownStates[key] != true ? "cyan" : "orange"
-          }"></div>`;
-        } else if (["systemState"].indexOf(key) >= 0) {
-          state =
-            knownStates[key] === SysState.Primary
-              ? "PRIMARY"
-              : knownStates[key] === SysState.Secondary
-              ? "SECONDARY"
-              : "UNKNOWN";
-        } else if (
-          ["last_db_power", "lastPing", "contactor_generator_time"].indexOf(
-            key
-          ) >= 0
+        const knownStates = self.knownStates as any;
+        lines.push(
+          '<div style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: flex-start; align-items: flex-start; align-content: flex-start; gap: 10px; ">'
+        );
+        lines.push(
+          "<style>.item-ball { width: 15px; height: 15px; border-radius: 50%; }</style>"
+        );
+        lines.push(
+          "<style>.item { background: white; border-radius: 10px; box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1); padding: 20px; }</style>"
+        );
+        lines.push(
+          "<style>.item h5 { display: block; margin: 0; margin-bottom: 10px; font-size: 20px; }</style>"
+        );
+        lines.push("<style>.item > br { display: none; }</style>");
+        lines.push(
+          "<style>.item > .litem { display: block; padding-bottom: 7px; }</style>"
+        );
+        lines.push(
+          "<style>.item > .litem > b, .item > .litem > span { display: inline-block; }</style>"
+        );
+        lines.push("<style>.item > .litem > span { float: right }</style>");
+        lines.push('<div class="item">');
+        lines.push("<h5>KNOWN STATES</h5>");
+        for (let key of Object.keys(knownStates)) {
+          let state: any = undefined;
+          if (Tools.isBoolean(knownStates[key])) {
+            state = `<div class="item-ball" style="background: ${
+              knownStates[key] != true ? "cyan" : "orange"
+            }"></div>`;
+          } else if (["systemState"].indexOf(key) >= 0) {
+            state =
+              knownStates[key] === SysState.Primary
+                ? "PRIMARY"
+                : knownStates[key] === SysState.Secondary
+                ? "SECONDARY"
+                : "UNKNOWN";
+          } else if (
+            ["last_db_power", "lastPing", "contactor_generator_time"].indexOf(
+              key
+            ) >= 0
+          ) {
+            state = new Date(knownStates[key]).toLocaleString();
+          } else if (Tools.isNumber(knownStates[key])) {
+            state = knownStates[key].toString();
+          } else {
+            state = knownStates[key];
+          }
+
+          lines.push(
+            `<div class="litem"><b>${key}</b>: <span>${
+              state || "UNKNOWN"
+            }</span></div>`
+          );
+        }
+        lines.push(
+          `<div class="litem"><b>TIME</b>: <span>${new Date().toLocaleString()}</span></div>`
+        );
+        lines.push("</div>");
+        lines.push('<div class="item">');
+        const loadSheddingState = self.loadSheddingState as any;
+        lines.push("<h5>LOAD SHEDDING</h5>");
+        for (let key of Object.keys(loadSheddingState)) {
+          let state: any = undefined;
+          if (Tools.isBoolean(loadSheddingState[key])) {
+            state = `<div class="item-ball" style="background: ${
+              loadSheddingState[key] !== true ? "green" : "red"
+            }"></div>`;
+          } else if (Tools.isNumber(loadSheddingState[key])) {
+            state = loadSheddingState[key].toString();
+          } else {
+            state = loadSheddingState[key];
+          }
+
+          lines.push(
+            `<div class="litem"><b>${key}</b>: <span>${
+              state || "UNKNOWN"
+            }</span></div>`
+          );
+        }
+        lines.push("</div>");
+        lines.push('<div class="item">');
+        const inputs = self.inputs.getState() as any;
+        lines.push("<h5>INPUTS</h5>");
+        for (let key of Object.keys(inputs)) {
+          let state: any = undefined;
+          if (Tools.isBoolean(inputs[key])) {
+            state = `<div class="item-ball" style="background: ${
+              inputs[key] == true ? "green" : "red"
+            }"></div>`;
+          } else if (Tools.isNumber(inputs[key])) {
+            state = inputs[key].toString();
+          } else {
+            state = inputs[key];
+          }
+
+          lines.push(
+            `<div class="litem"><b>${key}</b>: <span>${
+              state || "UNKNOWN"
+            }</span></div>`
+          );
+        }
+        lines.push("</div>");
+        lines.push('<div class="item">');
+        const outputs = self.outputs.getState() as any;
+        lines.push("<h5>OUTPUTS</h5>");
+        for (let key of Object.keys(outputs)) {
+          let state: any = undefined;
+          if (Tools.isBoolean(outputs[key])) {
+            state = `<div class="item-ball" style="background: ${
+              outputs[key] == true ? "green" : "red"
+            }"></div>`;
+          } else if (Tools.isNumber(outputs[key])) {
+            state = outputs[key].toString();
+          } else {
+            state = outputs[key];
+          }
+
+          lines.push(
+            `<div class="litem"><b>${key}</b>: <span>${
+              state || "UNKNOWN"
+            }</span></div>`
+          );
+        }
+        lines.push("</div>");
+        // lines.push('<div class="item">');
+        // lines.push('<h5>INFO</h5>');
+        // lines.push(
+        //   '<b style="display: inline-block;">TIME:</b>' +
+        //     new Date().toLocaleString()
+        // );
+        // lines.push('</div>');
+        lines.push('<div class="item">');
+        lines.push("<h5>SYSTEM STATE LOGS</h5>");
+        // loop through _latestSystemBusyPoint with an index, make the first item bold, and the rest normal
+        for (
+          let index = 0;
+          index < self._latestSystemBusyPoint.length;
+          index++
         ) {
-          state = new Date(knownStates[key]).toLocaleString();
-        } else if (Tools.isNumber(knownStates[key])) {
-          state = knownStates[key].toString();
-        } else {
-          state = knownStates[key];
+          if (self._latestSystemBusyPoint[index].indexOf(":") < 0) continue;
+          let workingContent = self._latestSystemBusyPoint[index].split(":");
+          if (workingContent.length < 2) continue;
+          lines.push(
+            `<div class="litem"><b>${workingContent
+              .splice(0, 1)[0]
+              .trim()}</b>: <span>${workingContent
+              .join(":")
+              .trim()}</span></div>`
+          );
         }
-
-        lines.push(
-          `<div class="litem"><b>${key}</b>: <span>${
-            state || "UNKNOWN"
-          }</span></div>`
+        lines.push("</div>");
+        lines.push("</div>");
+        reply.send(
+          '<html><head><meta http-equiv="refresh" content="1"></head><body style="background: rgb(242, 242, 242);">' +
+            lines.join("<br />") +
+            "</body></html>"
         );
       }
-      lines.push(
-        `<div class="litem"><b>TIME</b>: <span>${new Date().toLocaleString()}</span></div>`
-      );
-      lines.push("</div>");
-      lines.push('<div class="item">');
-      const loadSheddingState = self.loadSheddingState as any;
-      lines.push("<h5>LOAD SHEDDING</h5>");
-      for (let key of Object.keys(loadSheddingState)) {
-        let state: any = undefined;
-        if (Tools.isBoolean(loadSheddingState[key])) {
-          state = `<div class="item-ball" style="background: ${
-            loadSheddingState[key] !== true ? "green" : "red"
-          }"></div>`;
-        } else if (Tools.isNumber(loadSheddingState[key])) {
-          state = loadSheddingState[key].toString();
-        } else {
-          state = loadSheddingState[key];
-        }
-
-        lines.push(
-          `<div class="litem"><b>${key}</b>: <span>${
-            state || "UNKNOWN"
-          }</span></div>`
-        );
-      }
-      lines.push("</div>");
-      lines.push('<div class="item">');
-      const inputs = self.inputs.getState() as any;
-      lines.push("<h5>INPUTS</h5>");
-      for (let key of Object.keys(inputs)) {
-        let state: any = undefined;
-        if (Tools.isBoolean(inputs[key])) {
-          state = `<div class="item-ball" style="background: ${
-            inputs[key] == true ? "green" : "red"
-          }"></div>`;
-        } else if (Tools.isNumber(inputs[key])) {
-          state = inputs[key].toString();
-        } else {
-          state = inputs[key];
-        }
-
-        lines.push(
-          `<div class="litem"><b>${key}</b>: <span>${
-            state || "UNKNOWN"
-          }</span></div>`
-        );
-      }
-      lines.push("</div>");
-      lines.push('<div class="item">');
-      const outputs = self.outputs.getState() as any;
-      lines.push("<h5>OUTPUTS</h5>");
-      for (let key of Object.keys(outputs)) {
-        let state: any = undefined;
-        if (Tools.isBoolean(outputs[key])) {
-          state = `<div class="item-ball" style="background: ${
-            outputs[key] == true ? "green" : "red"
-          }"></div>`;
-        } else if (Tools.isNumber(outputs[key])) {
-          state = outputs[key].toString();
-        } else {
-          state = outputs[key];
-        }
-
-        lines.push(
-          `<div class="litem"><b>${key}</b>: <span>${
-            state || "UNKNOWN"
-          }</span></div>`
-        );
-      }
-      lines.push("</div>");
-      // lines.push('<div class="item">');
-      // lines.push('<h5>INFO</h5>');
-      // lines.push(
-      //   '<b style="display: inline-block;">TIME:</b>' +
-      //     new Date().toLocaleString()
-      // );
-      // lines.push('</div>');
-      lines.push('<div class="item">');
-      lines.push("<h5>SYSTEM STATE LOGS</h5>");
-      // loop through _latestSystemBusyPoint with an index, make the first item bold, and the rest normal
-      for (let index = 0; index < self._latestSystemBusyPoint.length; index++) {
-        if (self._latestSystemBusyPoint[index].indexOf(":") < 0) continue;
-        let workingContent = self._latestSystemBusyPoint[index].split(":");
-        if (workingContent.length < 2) continue;
-        lines.push(
-          `<div class="litem"><b>${workingContent
-            .splice(0, 1)[0]
-            .trim()}</b>: <span>${workingContent.join(":").trim()}</span></div>`
-        );
-      }
-      lines.push("</div>");
-      lines.push("</div>");
-      reply.send(
-        '<html><head><meta http-equiv="refresh" content="1"></head><body style="background: rgb(242, 242, 242);">' +
-          lines.join("<br />") +
-          "</body></html>"
-      );
-    });
+    );
   }
 
   private async generator() {
