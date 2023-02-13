7221e9808f11389aa9b8b22603c1b04048333f92
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-02-13 20:22:38 +0200

    added revolving log files during runs as well

--- src/plugins/log-ats-controller/plugin.ts
+++ src/plugins/log-ats-controller/plugin.ts
@@ -13,6 +13,7 @@ export class Logger extends LoggerBase<PluginConfig> {
   private logStream: WriteStream | null = null;
   private logTimeout: NodeJS.Timeout | undefined = undefined;
   private firstLog: boolean = true;
+  private logLinesWritten: number = 0;
   private async logTimeoutHandler() {
     if (!Tools.isNullOrUndefined(this.logStream)) {
       this.logStream.close();
@@ -20,11 +21,39 @@ export class Logger extends LoggerBase<PluginConfig> {
     }
     this.logTimeout = undefined;
   }
+  private async revolveLog() {
+    const logFile = await (await this.getPluginConfig()).logFile!;
+    const logFilePath = join(this.cwd, logFile);
+    if (existsSync(logFilePath)) {
+      let counter = 0;
+      let tLogfilePath = `${logFilePath}.0`;
+      while (existsSync(tLogfilePath)) {
+        tLogfilePath = `${join(this.cwd, logFile)}.${counter}`;
+        counter++;
+      }
+      renameSync(logFilePath, tLogfilePath);
+      this.logLinesWritten = 0;
+      this.firstLog = false;
+    }
+  }
+  private async writeLogLine(log: string): Promise<boolean> {
+    this.logLinesWritten++;
+    this.logStream!.write(new Date().toISOString());
+    this.logStream!.write(": ");
+    this.logStream!.write(log);
+    this.logStream!.write("\n");
+
+    if (this.logLinesWritten > 100000) {
+      clearTimeout(this.logTimeout);
+      await this.logTimeoutHandler();
+      await this.revolveLog();
+      return true;
+    }
+    return false;
+  }
   private async writeLog(log: string) {
     if (!Tools.isNullOrUndefined(this.logStream)) {
-      this.logStream.write(new Date().toISOString());
-      this.logStream.write(log);
-      this.logStream.write("\n");
+      if (await this.writeLogLine(log)) return;
       clearTimeout(this.logTimeout);
       this.logTimeout = setTimeout(this.logTimeoutHandler.bind(this), 1000);
       return;
@@ -33,21 +62,10 @@ export class Logger extends LoggerBase<PluginConfig> {
     const logFilePath = join(this.cwd, logFile);
 
     if (this.firstLog) {
-      if (existsSync(logFilePath)) {
-        let counter = 0;
-        let tLogfilePath = `${logFilePath}.0`;
-        while (existsSync(tLogfilePath)) {
-          tLogfilePath = `${join(this.cwd, logFile)}.${counter}`;
-          counter++;
-        }
-        renameSync(logFilePath, tLogfilePath);
-      }
+      await this.revolveLog();
     }
-    this.firstLog = false;
     this.logStream = createWriteStream(logFilePath, { encoding: "utf8" });
-    this.logStream.write(new Date().toISOString());
-    this.logStream.write(log);
-    this.logStream.write("\n");
+    if (await this.writeLogLine(log)) return;
     this.logTimeout = setTimeout(this.logTimeoutHandler.bind(this), 1000);
   }
 
